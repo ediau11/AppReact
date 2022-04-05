ddcc3ff29677e7cc3dfccf8b64e23e644b9d463a
Eduardo Arcila <eduardocila11@gmail.com>
2022-04-05 01:31:30 -0300

    agregando estilos a mi carrito de compras

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,7 +3,57 @@ import React from "react";
 function inicio() {
   return (
     <>
-      <body className="color">
+      <div class="row rowInicio">
+        <div class="col">
+          <div className="container-fluid  p-5 my-5  contenedorInicio">
+            <img
+              className="imagenesInicio"
+              src="https://i.postimg.cc/zBDL4G2q/Imagen1.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="col">
+          <div className="container-fluid  p-5 my-5  contenedorInicio">
+            <h2 className=" text-center tituloImagenes">
+              Cautiva tus sentidos:
+            </h2>
+            <p className=" textoImagenes">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sed
+              laborum, cumque harum impedit culpa. Dicta blanditiis laboriosam,
+              temporibus ad minus non qui consequatur aliquid, accusamus quaerat
+              laborum eaque aperiam!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row rowInicio">
+        <div class="col">
+          <div className="container-fluid  p-5 my-5  contenedorInicio">
+            <h2 className=" text-center tituloImagenes">
+              Cautiva tus sentidos:
+            </h2>
+            <p className=" textoImagenes">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sed
+              laborum, cumque harum impedit culpa. Dicta blanditiis laboriosam,
+              temporibus ad minus non qui consequatur aliquid, accusamus quaerat
+              laborum eaque aperiam!
+            </p>
+          </div>
+        </div>
+        <div class="col">
+          <div className="container-fluid  p-5 my-5  contenedorInicio">
+            <img
+              className="imagenesInicio"
+              src="https://i.postimg.cc/zBDL4G2q/Imagen1.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* <body className="color">
         <div class="row galeria">
           <div class="col">
             <img
@@ -42,7 +92,7 @@ function inicio() {
             <img src="https://i.postimg.cc/2jMJwc5S/Imagen3.jpg" alt="" />
           </div>
         </div>
-      </body>
+      </body> */}
     </>
   );
 }
