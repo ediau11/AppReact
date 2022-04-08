7f731a3cec342d36a366b1fc9ffd4bf87aa54a9d
Eduardo Arcila <eduardocila11@gmail.com>
2022-04-08 02:12:03 -0300

    Modificando estilos

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -1,98 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function inicio() {
   return (
     <>
-      <div class="row rowInicio">
+      <div className="container-fluid contenedorImagen">
+        <img
+          className="img-fluid imagenInicio"
+          src="https://www.pioneerdj.com/-/media/project/pioneer/pioneerdj/home/carousel/2022/ddj-rev1_web_home-crousel_1440x600.jpg"
+          alt=""
+        />
+
+        <div className="texto-encima">
+          <h1
+            className="heroMain__title heading heading--1"
+            id="hero-title-tag"
+          >
+            DDJ-REV1
+          </h1>
+        </div>
+        <div className="texto-encima2">
+          <h3
+            className="heroMain__title heading heading--1 "
+            id="hero-title-tag"
+          >
+            Join the REVolution
+          </h3>
+        </div>
+        <div className="texto-encima3 ">
+          <Link to={"/productos"}>
+            <button className="btn btn-primary botonInicio">Detalles</button>
+          </Link>
+        </div>
+      </div>
+      <h1 className="text-center">Nuestras Marcas</h1>
+
+      <div class="row">
         <div class="col">
-          <div className="container-fluid  p-5 my-5  contenedorInicio">
+          <div className="Container">
             <img
-              className="imagenesInicio"
-              src="https://i.postimg.cc/zBDL4G2q/Imagen1.jpg"
+              src="https://i.postimg.cc/Gm55yztk/sophisticated-design-1053x1053-sp2.jpg"
               alt=""
+              className="contenedorImagenInicio"
             />
           </div>
         </div>
         <div class="col">
-          <div className="container-fluid  p-5 my-5  contenedorInicio">
-            <h2 className=" text-center tituloImagenes">
-              Cautiva tus sentidos:
-            </h2>
-            <p className=" textoImagenes">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sed
-              laborum, cumque harum impedit culpa. Dicta blanditiis laboriosam,
-              temporibus ad minus non qui consequatur aliquid, accusamus quaerat
-              laborum eaque aperiam!
-            </p>
+          <div className="Container">
+            <img
+              src="https://i.postimg.cc/Gm55yztk/sophisticated-design-1053x1053-sp2.jpg"
+              alt=""
+              className="contenedorImagenInicio"
+            />
+          </div>
+        </div>
+        <div class="col">
+          <div className="Container">
+            <img
+              src="https://i.postimg.cc/Gm55yztk/sophisticated-design-1053x1053-sp2.jpg"
+              alt=""
+              className="contenedorImagenInicio"
+            />
           </div>
         </div>
       </div>
-
-      <div class="row rowInicio">
-        <div class="col">
-          <div className="container-fluid  p-5 my-5  contenedorInicio">
-            <h2 className=" text-center tituloImagenes">
-              Cautiva tus sentidos:
-            </h2>
-            <p className=" textoImagenes">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sed
-              laborum, cumque harum impedit culpa. Dicta blanditiis laboriosam,
-              temporibus ad minus non qui consequatur aliquid, accusamus quaerat
-              laborum eaque aperiam!
-            </p>
-          </div>
-        </div>
-        <div class="col">
-          <div className="container-fluid  p-5 my-5  contenedorInicio">
-            <img
-              className="imagenesInicio"
-              src="https://i.postimg.cc/zBDL4G2q/Imagen1.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-
-      {/* <body className="color">
-        <div class="row galeria">
-          <div class="col">
-            <img
-              className=""
-              src="https://i.postimg.cc/zBDL4G2q/Imagen1.jpg"
-              alt=""
-            />
-          </div>
-          <div class="col">
-            <img
-              className=""
-              src="https://i.postimg.cc/76qBcSyb/Imagen2.jpg"
-              alt=""
-            />
-          </div>
-          <div class="col">
-            <img src="https://i.postimg.cc/2jMJwc5S/Imagen3.jpg" alt="" />
-          </div>
-        </div>
-        <div class="row galeria">
-          <div class="col">
-            <img
-              className=""
-              src="https://i.postimg.cc/MKGT7GvS/Imagen4.jpg"
-              alt=""
-            />
-          </div>
-          <div class="col galeria">
-            <img
-              className=""
-              src="https://i.postimg.cc/mkpTXQtG/Imagen5.jpg"
-              alt=""
-            />
-          </div>
-          <div class="col">
-            <img src="https://i.postimg.cc/2jMJwc5S/Imagen3.jpg" alt="" />
-          </div>
-        </div>
-      </body> */}
     </>
   );
 }
