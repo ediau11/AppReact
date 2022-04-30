e391d8aec1a738d4d39abd5d6db6118cb5f911b4
Eduardo Arcila <eduardocila11@gmail.com>
2022-04-30 02:07:45 -0300

    arreglando ruta para confirmacion de orden

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -5,7 +5,6 @@ import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 import OrdenConfirm from "../OrdenConfirm/OrdenConfirm";
 import CartEmpty from "../CartEmpty/CartEmpty";
-import { useNavigate } from "react-router-dom";
 
 const Formulario = () => {
   const [refOrder, setRefOrder] = useState();
@@ -38,7 +37,7 @@ const Formulario = () => {
   };
 
   if (refOrder) {
-    return <OrdenConfirm refOrder={refOrder} />;
+    return <OrdenConfirm refOrder={refOrder} sendOrder={sendOrder} />;
   }
 
   return (
@@ -84,25 +83,6 @@ const Formulario = () => {
           </div>
         </div>
       )}
-      {/* 
-      {refOrder && (
-        <div className="text-center p-5 mt-5">
-          <h1 className="textoCompra">Listo!</h1>
-
-          <h3 className="textoCompra mt-5">Muchas gracias por su compra!</h3>
-
-          <p className="textoCompra mt margenFormulario">
-            Su orden {refOrder.id} esta siendo procesada
-          </p>
-
-          <button
-            className="botonAnimationCarrito mt-5 textoCompra"
-            onClick={() => navigate("/Productos")}
-          >
-            Volver a comprar
-          </button>
-        </div>
-      )} */}
     </>
   );
 };
--- src/components/OrdenConfirm/OrdenConfirm.jsx
+++ src/components/OrdenConfirm/OrdenConfirm.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const OrdenConfirm = (refOrder) => {
+const OrdenConfirm = ({ refOrder }) => {
   const navigate = useNavigate();
   return (
     <>
       <div className="text-center p-5 mt-5">
         <h1 className="textoCompra">Listo!</h1>
 
-        <h3 className="textoCompra mt-5">Muchas gracias por su compra!</h3>
+        <h3 className="textoCompra mt-5">Muchas gracias por su compra! </h3>
 
         <p className="textoCompra mt margenFormulario">
-          Su orden {refOrder.id} esta siendo procesada
+          Tu numero de pedido es: {refOrder.id}
         </p>
 
         <button
